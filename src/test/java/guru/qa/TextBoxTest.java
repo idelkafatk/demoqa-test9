80195647880dde89b6900fda8487c515962232a3
idelfatkhullin <maximbravin@gmail.com>
2022-11-19 11:15:03 +0600

    added text box test

---
+++ src/test/java/guru/qa/TextBoxTest.java
@@ -0,0 +1,25 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("name");
+        $("#userEmail").setValue("email@ru.com");
+        $("#currentAddress").setValue("address");
+        $("#permanentAddress").setValue("permanent");
+        $("#submit").scrollIntoView(true).click();
+
+        $("#output #name").shouldHave(text("name"));
+        $("#output #email").shouldHave(text("email@ru.com"));
+        $("#output #currentAddress").shouldHave(text("address"));
+        $("#output #permanentAddress").shouldHave(text("permanent"));
+    }
+}
